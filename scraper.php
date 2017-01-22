ae0dc21f004618ebbab65d265e3fbc10d913de50
mzdemio <demiomz@gmail.com>
2017-01-23 12:59:13 +1300

    Test

--- scraper.php
+++ scraper.php
@@ -4,9 +4,9 @@ $xml = simplexml_load_file("http://www.dbs.com/ratesonline/xml/Pages/XFXBRA.xml"
 foreach ($xml->xpath("/rates/record/currency") as $child) {
     if($child->attributes()->value == "New Zealand Dollar"){
         $parent = $child->xpath("parent::*");
-        $sell = $parent[0]->xpath("record_1/selling_ttod");
+        $sell = $parent[0]->xpath("record_2/selling_ttod");
         $rate = floatval($sell[0]->attributes()->value[0]);
-        $total = number_format(190000/$rate,2);
+        $total = number_format(1/$rate,2);
         $record = array( 'currency' => 'NZD', 'Total' => $total);
         print "Total : $total \n"; 
     }
